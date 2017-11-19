a13ab5a9138a54df631d1cb8ad06c3983acaf3f9
hornig <chefkueche@hornshouse.com>
2017-11-19 20:45:45 +0100

    decoding the bits

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from scipy.io.wavfile import write as sc_write
 
 
 # inspired by
-## https://github.com/osmocom/rtl-sdr/blob/master/src/rtl_fm.c
+# https://github.com/osmocom/rtl-sdr/blob/master/src/rtl_fm.c
 # thank you!
 
 def loading_file(filename):
@@ -24,7 +24,6 @@ def loading_file(filename):
     return signal
 
 
-
 def fm_demod(signal):
     pre_r = 0
     pre_j = 0
@@ -34,7 +33,7 @@ def fm_demod(signal):
 
     # low-passing
     lp = low_pass(signal)
-    lp_len = len(lp) # must stay double the size of result, due to later for loop
+    lp_len = len(lp)  # must stay double the size of result, due to later for loop
 
     result = np.zeros(len(lp)//2)
 
@@ -52,10 +51,8 @@ def fm_demod(signal):
 
     pre_r = lp[lp_len - 2]
     pre_j = lp[lp_len - 1]
-    result_len = lp_len // 2
-
+    # result_len = lp_len // 2
     return result
-
 
 
 def multiply(ar, aj, br, bj):
@@ -64,26 +61,24 @@ def multiply(ar, aj, br, bj):
     return cr, cj
 
 
-
 def polar_discriminant(ar, aj, br, bj):
-    cr , cj = multiply(ar, aj, br, -bj)
+    cr, cj = multiply(ar, aj, br, -bj)
     angle = np.arctan2(cj, cr)
-    return (angle / np.pi * (1<<14))
-    #return (angle * 180.0 / np.pi)
-
+    return (angle / np.pi * (1 << 14))
+    # return (angle * 180.0 / np.pi)
 
 
 def low_pass(signal):
     # simple square window FIR
 
-    lowpassed = np.zeros(len(signal)//downsample*2)
+    lowpassed = np.zeros(len(signal) // downsample*2)
 
     # needs to be go outside this function
     now_r = 0
     now_j = 0
 
-    i=0
-    i2=0
+    i = 0
+    i2 = 0
 
     prev_index = 0
 
@@ -97,15 +92,13 @@ def low_pass(signal):
         if (prev_index < downsample):
             continue
 
-        lowpassed[i2]= now_r
+        lowpassed[i2] = now_r
         lowpassed[i2 + 1] = now_j
         prev_index = 0
         now_r = 0
         now_j = 0
         i2 += 2
-
-    lp_len = i2
-
+    # lp_len = i2
     return lowpassed
 
 
@@ -158,32 +151,112 @@ def low_pass_real(input):
     result_len = i2
     return result
 
+def decode_nrzi(nrzi):
+    code_bit = []
+    # starting bit. with NRZI it doesn't matter, if 0 or 1 at the beginning
+    code_bit.append(1)
+
+    for bit in range(1, len(nrzi)):
+        if nrzi[bit - 1] == nrzi[bit]:
+            code_bit.append(1)
+        elif nrzi[bit - 1] != nrzi[bit]:
+            code_bit.append(0)
+
+    return code_bit
+
+def find_bit_stuffing(code_bit):
+    stuffed_bit = np.zeros(len(code_bit), dtype=np.int)
+
+    counter = 0
+    for bit in range(len(code_bit)):
+
+        if counter == 5 and code_bit[bit] == 1:
+            # could be ending, because 6th bit is not 0 and could be intentially left 1, as expected for the frame end
+            stuffed_bit[bit] = 2
+
+        if counter == 5 and code_bit[bit] == 0:
+            # normal bit stuffing
+            stuffed_bit[bit] = 1
+
+        #print(bit, code_bit[bit], stuffed_bit[bit], counter)
+
+        if code_bit[bit] == 1:
+            counter += 1
+        if code_bit[bit] == 0:
+            counter = 0
+
+    return stuffed_bit
+
+def byte_splitter(code_bit, stuffed_bit):
+
+    i = 0
+    c = 0
+    stop = 0
+
+    byte = []
+
+    out_byte = []
+    out_byte_start = []
+
+    while i < len(code_bit) and stop == 0:
+
+        if c == 0:
+            out_byte_start.append(i)
+
+        if stuffed_bit[i] != 1:
+            byte.append(code_bit[i])
+            c += 1
+
+
+        if c == 8:
+            c = 0
+            out_byte.append(byte)
+            byte = []
+
+        if stuffed_bit[i] == 2:
+            byte.append(0)
+            out_byte.append(byte)
+            # because it will stop on the 1 and not on the following zero, so zero needs to be added
+            stop = 1
+
+
+        i += 1
+
+    return out_byte, out_byte_start
+
+def translate(input):
+    input_reverse = ""
+    for bit in range(len(input),0,-1):
+        input_reverse += str(input[bit-1])
+
+
+    out = input_reverse
+    n = int(out[:8], 2)
+    print(str(chr(n)), out)
 
 if __name__ == '__main__':
     # all the inputs
-    ## basics
+    # # basics
     filename_sample = os.path.join("samples", "SDRSharp_20170830_073907Z_145825000Hz_IQ_autogain.wav")
-    frequency_offset_sample = -1500.0
+    frequency_offset_sample = -1500
 
-    ## input data conversion
+    # # input data conversion
     samplerate = 2048000
     chunk_period = 4
     chunk_size = samplerate * 2 * chunk_period
     frequency_offset = 0
 
-    ## audio
-    rate_in = 22050 # bandpass
+    # # audio
+    rate_in = 22050  # bandpass
     downsample = (samplerate // rate_in + 1) * 2
     capture_rate = rate_in * downsample
     rate_out = rate_in
     rate_out2 = 22050
 
-
     # now, let's start this and have some fun
 
-
-    ## loading in the iq imput file
-    ## adjusting the uint values by -127 to match the recorded IQ values to reality
+    # # loading in the iq imput file
+    # # adjusting the uint values by -127 to match the recorded IQ values to reality
     if len(sys.argv) == 1:
         filename = filename_sample
         frequency_offset = frequency_offset_sample
@@ -193,10 +266,9 @@ if __name__ == '__main__':
     signal = loading_file(filename)
     signal = -127 + signal[:]
 
-
-    ## converting signal to complex signal, but chunkwise
+    # # converting signal to complex signal, but chunkwise
     for chunk in range(0, len(signal), chunk_size):
-        signal_chunk = signal[chunk + 0 : chunk + chunk_size : 2] + 1j*signal[chunk + 1 : chunk + chunk_size : 2]
+        signal_chunk = signal[chunk + 0: chunk + chunk_size: 2] + 1j*signal[chunk + 1: chunk + chunk_size: 2]
 
         if frequency_offset != 0.0:
             # in case you think there could be a doppler shift or you commanded an frequency offset for the recording
@@ -207,7 +279,6 @@ if __name__ == '__main__':
             signal_shifted = signal_chunk * frequency_correction
         else:
             signal_shifted = signal_chunk
-
 
         # now comes "the audio" part. it is inspired by the rtl_fm.exe.
         # for now, we downconvert. at a later point, we will keep the original sampling rate
@@ -220,10 +291,8 @@ if __name__ == '__main__':
 
     signal_demod = fm_demod(signal)
 
-
     deemph_a = int(round(1.0 / ((1.0 - np.exp(-1.0 / (rate_out * 75e-6))))))
     signal_deemphed = deemph_filter(signal_demod)
-
 
     signal_final = low_pass_real(signal_deemphed)
     signal_final = np.array(signal_final, dtype=np.int16)
@@ -240,12 +309,11 @@ if __name__ == '__main__':
     plt.show()
     '''
 
-
     # 1200 baud AFSK demodulator
 
-    ## inspired by
-    ## https://github.com/EliasOenal/multimon-ng/blob/master/demod_afsk12.c
-    ## https://sites.google.com/site/wayneholder/attiny-4-5-9-10-assembly-ide-and-programmer/bell-202-1200-baud-demodulator-in-an-attiny10
+    # # inspired by
+    # # https://github.com/EliasOenal/multimon-ng/blob/master/demod_afsk12.c
+    # # https://sites.google.com/site/wayneholder/attiny-4-5-9-10-assembly-ide-and-programmer/bell-202-1200-baud-demodulator-in-an-attiny10
 
     baudrate = 1200.0
     buffer_size = int(np.round(rate_out2 / baudrate))
@@ -268,10 +336,8 @@ if __name__ == '__main__':
         corr_space_i[i] = np.cos(space_angle)
         corr_space_q[i] = np.sin(space_angle)
 
-
     # nornalizing the signal between -1 and +1
     signal_normalized = np.divide(signal_final, 2**15)
-
 
     # comparing the signal to the cosine and sine parts to both frequencies in the "correlation lists"
     binary_filter = np.zeros(len(signal_normalized))
@@ -290,10 +356,9 @@ if __name__ == '__main__':
             corr_sq = corr_sq + signal_normalized[sample + sub] * corr_space_q[sub]
 
         binary_filter[sample] = (corr_mi ** 2 + corr_mq ** 2 - corr_si ** 2 - corr_sq ** 2)
-        #binary_filter[sample] = np.sign(binary_filter[sample])
+        # binary_filter[sample] = np.sign(binary_filter[sample])
 
     binary_filter = np.sign(binary_filter)
-
 
     # visual check of the fm demodulated output
     plt.plot(binary_filter, label="binary filter")
@@ -301,7 +366,6 @@ if __name__ == '__main__':
     plt.title("fm demodulated signal and binary filter")
     plt.legend()
     plt.show()
-
 
     # finding the starting flag
     starting_flag_start = [0, 1, 1, 1, 1, 1, 1]
@@ -321,9 +385,7 @@ if __name__ == '__main__':
 
     time1 = time.time()
     correlation_start = np.divide(np.correlate(binary_filter, needle_start, mode="same"), len(needle_start))
-    "it took me", time.time() - time1, "seconds"
-
-
+    print("it took me", time.time() - time1, "seconds")
 
     # finding the positions of the starting flags
     starting_flag_positions = []
@@ -348,17 +410,15 @@ if __name__ == '__main__':
         print("only one starting flag found at", starting_flag_positions[0])
 
     elif len(starting_flag_positions) >= 2:
-        flag_distance = 600 # just a gut feeling. works for me
+        flag_distance = 600  # just a gut feeling. works for me
         for flag in range(len(starting_flag_positions) - 1):
             if starting_flag_positions[flag + 1] - starting_flag_positions[flag] >= flag_distance:
                 starting_flag_found.append(starting_flag_positions[flag])
                 print("for starting flag #", flag, ", found the starting flag at position", starting_flag_positions[flag])
 
-
         if starting_flag_positions[-1] - starting_flag_positions[-2] >= flag_distance:
             starting_flag_found.append(starting_flag_positions[-1])
             print("found the starting flag at position", starting_flag_positions[-1])
-
 
     for flag in range(len(starting_flag_found)):
         # finding the end of the needle
@@ -380,8 +440,7 @@ if __name__ == '__main__':
         # updating the found start with the real start
         starting_flag_found[flag] = starting_flag_found[flag] + runner
 
-
-
+    print("full signal")
     # visual check for full signal
     plt.plot(binary_filter, label="binary filter")
     plt.plot(correlation_start, label="correlation of starting flag over binary filter")
@@ -390,6 +449,7 @@ if __name__ == '__main__':
     plt.show()
 
     # making bits and bytes
+    print(starting_flag_found)
     for flag in range(len(starting_flag_found)):
         nrzi = []
         nrzi_quality = []
@@ -399,23 +459,22 @@ if __name__ == '__main__':
         while binary_filter[starting_flag_found[flag] + start] != 1:
             start += 1
 
+        # creating the bits of the nrzi code
         step = 0
         runner = 0
         correlation_sum = 0
-        while step < 3000 and correlation_sum < len(needle_small):
+        while step < 900:# and correlation_sum < len(needle_small):
             bit_quality = np.mean(binary_filter[starting_flag_found[flag] + start + runner:
-            starting_flag_found[flag] + start + runner + buffer_size])
+                                                starting_flag_found[flag] + start + runner + buffer_size])
             bit_quality_next = np.mean(binary_filter[starting_flag_found[flag] + start + runner + buffer_size:
-            starting_flag_found[flag] + start + runner + buffer_size * 2])
-            #print(step, np.sign(bit_quality), bit_quality, runner)
+                                                     starting_flag_found[flag] + start + runner + buffer_size * 2])
+            # print(step, np.sign(bit_quality), bit_quality, runner)
             nrzi.append(np.sign(bit_quality))
             nrzi_quality.append(bit_quality)
 
-            if len(nrzi) >= 7:
-                correlation_sum = np.max(np.correlate(nrzi[-8: -1], needle_small, mode="same"))
 
             if np.sign(bit_quality) < np.sign(bit_quality_next):
-                #print("synching step at step", step)
+                # print("synching step at step", step)
                 runner += buffer_size // 2
                 while binary_filter[starting_flag_found[flag] + start + runner] != 1:
                     runner += 1
@@ -438,7 +497,22 @@ if __name__ == '__main__':
         plt.legend()
         plt.show()
 
-        print("for starting flag #", flag, ", this is your non-return-to-zero-inverted code", nrzi)
+        bit_stream = decode_nrzi(nrzi)
+        stuffed_bits = find_bit_stuffing(bit_stream)
+
+        plt.plot(bit_stream, "o-")
+        plt.plot(stuffed_bits, "*")
+        plt.plot(np.divide(nrzi_quality, np.max(nrzi_quality)))
+        plt.show()
+
+        byte, byte_start = byte_splitter(bit_stream, stuffed_bits)
+        for b in range(len(byte)):
+            translate(byte[b])
+
+
+        # https://matplotlib.org/users/text_intro.html
+
+        print("test")
 
 
     print("finished, for now")
