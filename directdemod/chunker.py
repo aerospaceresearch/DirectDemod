45d01788ae69f00e4e8926fccc860f7e6fc80ca4
7andahalf <vinayck.work@gmail.com>
2018-05-16 23:21:38 +0530

    chunker util
    - chunker util to help in chunking

--- directdemod/chunker.py
+++ directdemod/chunker.py
@@ -4,19 +4,37 @@ chunking helper
 import directdemod.constants as constants
 import math
 
+'''
+This object is just to help in chunking process
+It is responsible for creating chunks of the signal and store the info to be used later
+It can be helpful for avoiding border issues in filters and demods
+'''
 class chunker:
+	'''
+	# Description: initialize the object
+	# Inputs: commSignal object to be chunked, chunkSize, Outputs: -
+	'''
 	def __init__(self, sigsrc, chunkSize = constants.PROC_CHUNKSIZE):
 		self.__nChunks = math.ceil(sigsrc.length*1.0/chunkSize)
-		self.chunks = []
+		self.__chunks = []
 		i = 0
-		while(i + chunkSize < sigsrc.length):
-			self.chunks.append([i,i + chunkSize])
-			i += chunkSize 
-		if len(self.chunks) == 0:
-			self.chunks.append([0,sigsrc.length])
-		else:
-			if not self.chunks[-1][1] == sigsrc.length:
-				self.chunks.append([self.chunks[-1][1],sigsrc.length])
 
+		# create normal sized chunks
+		while(i + chunkSize < sigsrc.length):
+			self.__chunks.append([i,i + chunkSize])
+			i += chunkSize 
+
+		# has it exhaused the whole signal?
+		if len(self.__chunks) == 0:
+			self.__chunks.append([0,sigsrc.length])
+		else: # if not put the remaining as another smaller chunk
+			if not self.__chunks[-1][1] == sigsrc.length:
+				self.__chunks.append([self.__chunks[-1][1],sigsrc.length])
+
+	'''
+	# Description: get the created chunks
+	# Inputs: -, Outputs: chunks list
+	'''
+	@property
 	def getChunks(self):
-		return self.chunks
+		return self.__chunks
--- noaa_cli.py
+++ noaa_cli.py
@@ -18,7 +18,7 @@ if not (len(sys.argv) == 2):
 sigsrc = source.IQwav(sys.argv[1])
 audioOut = comm.commSignal(constants.NOAA_AUDSAMPRATE)
 
-for i in chunker.chunker(sigsrc).getChunks()[:1]:
+for i in chunker.chunker(sigsrc).getChunks[:1]:
 	sig = comm.commSignal(constants.IQ_SDRSAMPRATE, sigsrc.read(*i)).offsetFreq(constants.IQ_FREQOFFSET).filter(filters.blackmanHarris()).bwLim(constants.IQ_FMBW).funcApply(fmDemod.fmDemod().demod).bwLim(constants.NOAA_AUDSAMPRATE, True)
 	audioOut.extend(sig)
 
